104fcdafb556d4c7e997b7d97d08c588c3e8aa12
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 20:22:08 -0600

    Add files via upload

---
+++ Day-4-banker-roulette.py
@@ -0,0 +1,25 @@
+# Import the random module here
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random
+
+# lucky_winner = random.choice(names)
+
+# print(f"{lucky_winner} is going to buy the meal today!")
+
+# she said to use the len() that function can count items in a list
+# the choice does the same thing but also randomizes
+
+# num_items = len(names)
+# #  the -1 is for the last thing in the index list
+# random_choice = random.randint(0, num_items - 1)
+# lucky_winner = names[random_choice]
+# print(random_choice)
+# print(lucky_winner)
+
+# amanda, brent, clayton
